2392539f4f78a5641c772939b7d816b75e8b1be5
DianaKorotkaya <dkorotkaya92@gmail.com>
2024-02-27 22:53:24 +0300

    Дз 4
    To do list

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('#input');
+const conteiner = document.querySelector('.items');
 
+sendInput.addEventListener('keydown', function(){
+    const text = sendInput.value;
+
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('items');
+    newMessage.textContent = text;
+
+    if(text != ''){
+        conteiner.append(newMessage);
+    }
+    
+    sendInput.value = '';
+
+    const elements = document.querySelectorAll('div');
+
+    for (let elem of elements) {
+      elem.addEventListener('click', function(){
+        elem.classList.toggle('done');
+     });
+      }
+    })
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
